5dc707ee83961aeb6f1ac9b1c116b6e8434dba32
andrxuy <andresoto198@gmail.com>
2026-05-20 12:45:45 -0500

    Pelicula.java

--- Pelicula.java
+++ Pelicula.java
@@ -1,1 +1,31 @@
+package tallerOtoAndres;
+
+public class Pelicula {
+    private String nombre;
+    private String director;
+    private String genero;
+    private double duracion;
+
+    public Pelicula(String nombre, String director, String genero, double duracion) {
+        this.nombre = nombre;
+        this.director = director;
+        this.genero = genero;
+        this.duracion = duracion;
+    }
+
+    public String getNombre() {return this.nombre;}
+    public String getDirector() {return this.director;}
+    public String getGenero() {return this.genero;}
+    public double getDuracion() {return this.duracion;}
+
+    @Override
+    public String toString() {
+        return "Pelicula{" +
+                "nombre='" + nombre + '\'' +
+                ", director='" + director + '\'' +
+                ", genero='" + genero + '\'' +
+                ", duracion=" + duracion +
+                '}';
+    }
+}
 
